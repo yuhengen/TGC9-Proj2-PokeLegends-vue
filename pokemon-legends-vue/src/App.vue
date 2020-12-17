27be01a9a84bddf0be9abc7c692fad1dc41dde57
yuhengen <yuhengen@gmail.com>
2020-12-17 10:36:47 +0000

    Successful testing with vue and axios

---
+++ pokemon-legends-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Testing/>
+  </div>
+</template>
+
+<script>
+import Testing from './components/Testing'
+export default {
+  name: 'App',
+  components: {
+      Testing
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pokemon-legends-vue/src/components/Testing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>My Pokemon</h1>
+    <div v-for="p in poke_moves" v-bind:key="p._id">
+        <h2>{{p.pokemon_name}}</h2>
+        <ul>
+            <li>{{p.moveset[0].attack}}</li>
+            <li>{{p.moveset[1].move1}}</li>
+            <li>{{p.moveset[2].move2}}</li>
+            <li>{{p.moveset[3].ultimate}}</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data: function () {
+        return {
+            poke_moves:[]
+        }
+    },
+    created:async function(){
+        let response = await axios.get("https://3000-f3eac718-8094-4909-ae3d-71ff4f3b9110.ws-us03.gitpod.io/movesets/");
+        this.poke_moves = response.data
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style scoped>
+</style>
