a99a8c1f6f083ec2509e5e907b79cbe9eedd335d
yuhengen <yuhengen@gmail.com>
2020-12-20 05:42:40 +0000

    Made modifications to navbar, attempting to implement modals as login window

--- pokemon-legends-vue/src/App.vue
+++ pokemon-legends-vue/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
+    <!-- Start Navbar -->
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <div class="mx-auto">
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="#" class="ml-4 mr-4">Home</b-nav-item>
-            <b-nav-item href="#" class="ml-4 mr-4">About</b-nav-item>
+            <b-nav-item href="#" class="ml-5 mr-5">Home</b-nav-item>
+            <b-nav-item href="#" class="ml-5 mr-5">About</b-nav-item>
           </b-navbar-nav>
 
-          <b-nav-item href="#" class="ml-4 mr-4">
+          <b-nav-item href="#" class="ml-5 mr-5">
             <img
               src="https://i.pinimg.com/originals/50/e1/db/50e1db4684e6f697f93590950eb832f6.png"
               class="brand-logo"
@@ -19,12 +20,17 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto" v-if="username == ''">
-            <b-nav-item href="#" class="ml-4 mr-4" disabled>Pokédex</b-nav-item>
-            <b-nav-item href="#" class="ml-4 mr-4">Login</b-nav-item>
+            <b-nav-item href="#" class="ml-5 mr-5" disabled>Pokédex</b-nav-item>
+            <b-nav-item
+              href="#"
+              class="ml-5 mr-5"
+              v-b-modal.modal-prevent-closing
+              >Login</b-nav-item
+            >
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto" v-else>
-            <b-nav-item href="#" class="ml-4 mr-4">Pokédex</b-nav-item>
-            <b-nav-item-dropdown right class="ml-4 mr-4">
+            <b-nav-item href="#" class="ml-5 mr-5">Pokédex</b-nav-item>
+            <b-nav-item-dropdown right class="ml-5 mr-5">
               <!-- Using 'button-content' slot -->
               <template #button-content>
                 <em>User</em>
@@ -36,19 +42,20 @@
         </b-collapse>
       </div>
     </b-navbar>
+    <!-- End navbar -->
 
     <div class="container">
-      <GameWindow />
+      <LoginPage />
     </div>
   </div>
 </template>
 
 <script>
-import GameWindow from "./components/GameWindow";
+import LoginPage from "./components/LoginPage";
 export default {
   name: "App",
   components: {
-    GameWindow,
+    LoginPage,
   },
   data: function () {
     return {
@@ -84,7 +91,12 @@ export default {
 
 @media (min-width: 768px) {
   .nav-item {
-    width: 150px;
+  font-family: pokemonsolid;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  font-size: 30px !important;
+  color: rgb(255, 255, 255);
+    width: 130px;
+    text-align: center;
   }
 }
 </style>
---
+++ pokemon-legends-vue/src/components/LoginPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <label>Username</label>
+      <input type="text" v-model="username" />
+      <button @click="updateTask" class="mt-3">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      userdata: [],
+      username: "",
+    };
+  },
+  created: async function () {
+    let response = await axios.get(
+      "https://3000-f3eac718-8094-4909-ae3d-71ff4f3b9110.ws-us03.gitpod.io/userdata/"
+    );
+    this.userdata = response.data;
+  },
+};
+</script>
+
+<style scoped>
+</style>
