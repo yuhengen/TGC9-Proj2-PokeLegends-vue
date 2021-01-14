08442dda98a7a0d7e9a5ee931afbfb962f9007e7
yuhengen <yuhengen@gmail.com>
2021-01-14 20:07:12 +0000

    Added display of array of party pokemon

--- pokemon-legends-vue/src/components/gamewindow/Pokemon.vue
+++ pokemon-legends-vue/src/components/gamewindow/Pokemon.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="pokemon-display" class="d-flex justify-content-center align-items-center">
-    <div class="pokemon-list"></div>
-    <div class="pokemon-info"></div>
+  <div
+    id="pokemon-display"
+    class="d-flex justify-content-center align-items-center"
+  >
+    <div
+      id="pokemon-list"
+      class="d-flex flex-column flex-wrap justify-content-center align-items-center"
+    >
+      <div
+        v-for="(pokemon, i) in $store.state.userData.party_pokemon"
+        :key="i"
+        class="pokemon-bar d-flex align-items-center justify-content-around shadow"
+      >
+        <img
+          style="height: 80%"
+          :src="
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' +
+            pokemon.pokemon_id +
+            '.gif'
+          "
+        />
+        <span>Lv.{{ pokemon.lvl }} {{ pokemon.pokemon_name }}</span>
+      </div>
+    </div>
+    <div id="pokemon-info"></div>
   </div>
 </template>
 
@@ -18,14 +40,23 @@ export default {};
   height: 100%;
 }
 
-.pokemon-list {
-  width: 45%;
+#pokemon-list {
+  width: 48%;
   height: 90%;
-  background-color: red;
+  background-color: rgba(52, 58, 64, 0.9);
 }
 
-.pokemon-info {
-  width: 45%;
+.pokemon-bar {
+  width: 80%;
+  height: 15%;
+  background-color: lightblue;
+  color: black;
+  border-radius: 10px;
+  border: 2px black solid;
+}
+
+#pokemon-info {
+  width: 48%;
   height: 90%;
   background-color: blue;
 }
